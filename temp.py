ed6b06ccb4e3ffb2175282f88789771e82eb0e43
wuozio <31254657+wuozio@users.noreply.github.com>
2017-08-29 13:12:25 -0500

    Add files via upload
    tarea 1 hecha en clase.

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 #Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
 import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
-plt.plot([1,2,3,4])
+plt.plot([12,13,15,15,15,15,15,15,15,15,15,15,15,16,16,16,16,20,23,23,23,26])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Crecimiento del material X por dia [cm]')
+plt.ylabel('Numero de parientes [cm]')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
 plt.savefig('temp.png')
 #Con esta instruccion mostramos la figura. 
